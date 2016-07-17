81d5f20206ccc2d0769e1a0e28a3d4c97e5460d0
Marta Suarez Rodriguez <marta.suarez.rodriguez@gmail.com>
2016-07-17 16:31:32 +0200

    Trying full flow with EB.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+aws_config=".ebextensions/.config"
+sed -i "s/#psql-connection-string#/${CHUSRODRIGUEZ_PGSQL_CONNECTION_STRING}/g" "$aws_config"
+sed -i "s/#aws-access-key#/${AWS_ACCESS_KEY_ID_SES}/g" "$aws_config"
+sed -i "s/#aws-access-key#/${AWS_SECRET_ACCESS_KEY_SES}/g" "$aws_config"
+
+
+gem install dpl
+dpl --provider=elasticbeanstalk --access-key-id=$access_key --secret-access-key="$secret_key" --app="chusrodriguez-${environment}-application" --env="chusrodriguez-${environment}-environment" --region="${region}"
