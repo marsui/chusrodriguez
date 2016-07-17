f46d02e8ae36972d6ccb8d1186693fcf8c714dab
Marta Suarez Rodriguez <marta.suarez.rodriguez@gmail.com>
2016-07-17 17:34:12 +0200

    Testing deploy only of config and extensions.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,15 +10,18 @@ secret_key_ses=${AWS_SECRET_ACCESS_KEY_SES:-none}
 
 environment=${ENVIRONMENT:-thread}
 
-mkdir -p .ebextensions
-touch .ebextensions/.config
-echo "option_settings:" >> .ebextensions/.config
-echo "  - option_name: CHUSRODRIGUEZ_PGSQL_CONNECTION" >> .ebextensions/.config
-echo "    value: ${pgsql_connection}" >> .ebextensions/.config
-echo "  - option_name: AWS_ACCESS_KEY_ID" >> .ebextensions/.config
-echo "    value: ${access_key_ses}" >> .ebextensions/.config
-echo "  - option_name: AWS_SECRET_ACCESS_KEY" >> .ebextensions/.config
-echo "      value: ${secret_key_ses}" >> .ebextensions/.config
+mkdir -p .deploy/.ebextensions
+touch .deploy/.ebextensions/.config
+echo "option_settings:" >> .deploy/.ebextensions/.config
+echo "  - option_name: CHUSRODRIGUEZ_PGSQL_CONNECTION" >> .deploy/.ebextensions/.config
+echo "    value: ${pgsql_connection}" >> .deploy/.ebextensions/.config
+echo "  - option_name: AWS_ACCESS_KEY_ID" >> .deploy/.ebextensions/.config
+echo "    value: ${access_key_ses}" >> .deploy/.ebextensions/.config
+echo "  - option_name: AWS_SECRET_ACCESS_KEY" >> .deploy/.ebextensions/.config
+echo "      value: ${secret_key_ses}" >> .deploy/.ebextensions/.config
+
+cp Dockerrun.aws.json .deploy/
+cd .deploy/
 
 gem install dpl
 dpl --provider=elasticbeanstalk --access-key-id="$access_key" --secret-access-key="$secret_key" --app="chusrodriguez-${environment}-application" --env="chusrodriguez-${environment}-environment" --region="${region}" --bucket-name="chusrodriguez-deployments"
