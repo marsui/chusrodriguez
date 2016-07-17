e976985cba5a264118d2ddac2a8241f426547d29
Marta Suarez Rodriguez <marta.suarez.rodriguez@gmail.com>
2016-07-17 12:12:13 +0200

    Fix docker push .

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker login -e $REGISTRY_EMAIL -u $REGISTRY_USER -p $REGISTRY_PASSWORD
-docker push chusrodriguez/chusrodriguez-app:lastest
+docker login -u $REGISTRY_USER -p $REGISTRY_PASSWORD
+docker push marsui/chusrodriguez:lastest
