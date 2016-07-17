8a4280c529add913960b252b547fb73e0dd1dbdf
Marta Suarez Rodriguez <marta.suarez.rodriguez@gmail.com>
2016-07-17 15:54:35 +0200

    Merging wrong stashed changes.

--- infra/app/logs.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my_tf_log_bucket"
-  acl = "log-delivery-write"
-}
--- infra/app/variables.tf
+++ infra/app/variables.tf
@@ -1,19 +1,26 @@
+variable "db_name" {
+  default = "chusrodriguez"
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "azs" {
+  default = "eu-west-1a,eu-west-1b" // ,eu-west-1b,eu-west-1c
+}
+
+variable "subnet_cidr" {
+  default = "10.250.0.0/24,10.250.1.0/24"
+}
+
 variable "app-name" {
   default = "chusrodriguez"
 }
 
 variable "environment" {
-	default = "thread"
-}
-
-variable "region" {
-  default = "eu-west-1"
-}
-
-variable "azs" {
-  default = "eu-west-1a" // ,eu-west-1b,eu-west-1c
-}
-
-variable "bastion_ami" {
-  default = ""
+  default = "thread"
 }
