2b6bb0dc5430fcfc18ce8c1a793dc7b821bff5f0
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-18 18:53:27 +0200

    Fixing db-data docker build, env yarn.

--- scripts/components-up/data.sh
+++ scripts/components-up/data.sh
@@ -6,12 +6,12 @@ access_key=${AWS_ACCESS_KEY_ID:-none}
 secret_key=${AWS_SECRET_ACCESS_KEY:-none}
 region=${AWS_REGION:-eu-west-1}
 
-environment=${ENVIRONMENT:-thread}
+environment=${ENVIRONMENT:-yarn}
 
 db_host=($(node util/rds-endpoint.js))
 db_name=${DB_NAME:-chusrodriguez}
 db_user=${DB_USERNAME:-root}
 db_password=$DB_ROOT_PASSWORD
 
-docker build -t psql-loader:latest infra/db
+docker build -t psql-loader:latest infra/db-data
 docker run -ti -e PGSQL_USER=$db_user -e PGSQL_PASS=$db_password -e PGSQL_HOST=$db_host -e PGSQL_DB=$db_name psql-builder:latest
--- scripts/components-up/dns.sh
+++ scripts/components-up/dns.sh
@@ -10,7 +10,7 @@ region=${AWS_REGION:-eu-west-1}
 zone=${HOSTED_ZONE_ID:-none}
 cname=$(SHA=$sha node util/beanstalk-cname.js)
 
-environment=${ENVIRONMENT:-thread}
+environment=${ENVIRONMENT:-yarn}
 
 ./vendor/terraform/terraform get -update=true infra/dns
 ./vendor/terraform/terraform apply -var aws_region=$region -var beanstalk_cname=$cname -var zone_id=$zone -var environment=$environment -var aws_access_key=$access_key -var aws_secret_key=$secret_key -state $environment.tfstate infra/dns
