7c22d2c8d58540e7d97149a34d5f617a35284894
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-18 21:52:08 +0200

    Including docker images to see built ones.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-npm run install:bower
-npm run build
+#npm run install:bower
+#npm run build
 
 docker build -t marsui/chusrodriguez:latest .
 
 cd infra/db-data
 docker build -t psql-loader:latest .
+
+docker images
