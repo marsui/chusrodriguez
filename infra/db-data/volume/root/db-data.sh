1fb00a75f13116295f180bf03dfbc85fbb0dce81
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-19 14:36:58 +0200

    Including chusrodriguez db user upon ddl injection.

--- infra/db-data/volume/root/db-data.sh
+++ infra/db-data/volume/root/db-data.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 export PGPASSWORD=$PGSQL_PASS
+
+psql -h ${PGSQL_HOST} -U ${PGSQL_USER} -c "create user chusrodriguez with password '${DB_CHUSRODRIGUEZ_PASSWORD}'" -d $PGSQL_DB
 psql -h ${PGSQL_HOST} -U ${PGSQL_USER} -f ./data.sql -d $PGSQL_DB
