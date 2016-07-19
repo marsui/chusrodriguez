fa70597227c0296de2a5071bd70f4b42df2bff57
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-19 14:44:38 +0200

    AB anytime.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -9,16 +9,16 @@ environment=${ENVIRONMENT:-thread}
 
 # Verify environment existence
 
-check=$(ENVIRONMENT=$environment node util/rds-check.js)
-if [[ $check == 'false' ]]
-then
+#check=$(ENVIRONMENT=$environment node util/rds-check.js)
+#if [[ $check == 'false' ]]
+#then
   bash scripts/components-up/app.sh > .terraform-output.txt
 
   echo 'Inserting DDL and sample data'
   bash scripts/components-up/data.sh
   echo 'finished db deployment'
-elif [[ $check != 'true' ]]
-then
-  echo $check
-  exit 1
-fi
+#elif [[ $check != 'true' ]]
+#then
+#  echo $check
+#  exit 1
+#fi
