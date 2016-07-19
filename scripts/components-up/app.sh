8bea3566a4b28fd738575b088717a8740edf000d
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-19 12:54:08 +0200

    Including zone-id on terraform script.

--- scripts/components-up/app.sh
+++ scripts/components-up/app.sh
@@ -7,9 +7,10 @@ secret_key=${AWS_SECRET_ACCESS_KEY:-none}
 region=${AWS_REGION:-eu-west-1}
 
 environment=${ENVIRONMENT:-thread}
+zone_id=${HOSTED_ZONE_ID:-none}
 
 state_bucket=${STATE_BUCKET}
 
 ./vendor/terraform/terraform remote config -backend s3 -backend-config="bucket=$state_bucket" -backend-config="key=states/${environment}.tfstate" -backend-config=region="${region}"
 ./vendor/terraform/terraform get -update=true infra/terraform
-./vendor/terraform/terraform apply -var aws_region=$region -var db_name=$db_name -var db_user=$db_user -var db_password=$db_password -var environment=$environment -var aws_access_key=$access_key -var aws_secret_key=$secret_key infra/terraform
+./vendor/terraform/terraform apply -var aws_region=$region -var db_name=$db_name -var db_user=$db_user -var db_password=$db_password -var zone_id=$zone_id -var environment=$environment -var aws_access_key=$access_key -var aws_secret_key=$secret_key infra/terraform
