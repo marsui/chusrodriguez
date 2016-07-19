f32ddc9aaf177e5154a6f5c1913b7ff5a54ccddd
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-19 18:22:58 +0200

    Fixing default user name.

--- scripts/components-up/data.sh
+++ scripts/components-up/data.sh
@@ -10,7 +10,7 @@ environment=${ENVIRONMENT:-thread}
 
 db_host=($(ENVIRONMENT=$environment node util/rds-endpoint.js))
 db_name=${DB_NAME:-chusrodriguez}
-db_user=${DB_USERNAME:-root}
+db_user=${DB_USERNAME:-chusrodriguez}
 db_password=$DB_ROOT_PASSWORD
 
 docker run -ti -e PGSQL_USER=$db_user -e PGSQL_PASS=$db_password -e PGSQL_HOST=$db_host -e PGSQL_DB=$db_name psql-loader:latest
