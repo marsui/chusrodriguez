3917407d5032dc2f59f48d55a19413258db02ce1
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-19 14:37:17 +0200

    Restablishing ddl only if db infra is launching.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -13,12 +13,12 @@ check=$(ENVIRONMENT=$environment node util/rds-check.js)
 if [[ $check == 'false' ]]
 then
   bash scripts/components-up/app.sh > .terraform-output.txt
+
+  echo 'Inserting DDL and sample data'
+  bash scripts/components-up/data.sh
+  echo 'finished db deployment'
 elif [[ $check != 'true' ]]
 then
   echo $check
   exit 1
 fi
-
-echo 'Inserting DDL and sample data'
-bash scripts/components-up/data.sh
-echo 'finished db deployment'
