0ffe7ade276775491412930d4782e3b8535146f0
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-07-19 19:36:33 +0200

    Connecting app and database.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,18 +5,22 @@ sha=$(git rev-parse --short HEAD)
 access_key=${AWS_ACCESS_KEY_ID:-none}
 secret_key=${AWS_SECRET_ACCESS_KEY:-none}
 region=${AWS_REGION:-eu-west-1}
+environment=${ENVIRONMENT:-thread}
 
-pgsql_connection=${CHUSRODRIGUEZ_PGSQL_CONNECTION_STRING:-none}
+db_host="db-${environment}-db.tallerchusrodriguez.com"
+db_name=${DB_NAME:-chusrodriguez}
+db_user=${DB_USERNAME:-chusrodriguez}
+db_password=$DB_ROOT_PASSWORD
+
 access_key_ses=${AWS_ACCESS_KEY_ID_SES:-none}
 secret_key_ses=${AWS_SECRET_ACCESS_KEY_SES:-none}
 
-environment=${ENVIRONMENT:-thread}
 
 mkdir -p .ebextensions
 touch .ebextensions/.config
 echo "option_settings:" >> .ebextensions/.config
 echo "  - option_name: CHUSRODRIGUEZ_PGSQL_CONNECTION" >> .ebextensions/.config
-echo "    value: \"${pgsql_connection}\"" >> .ebextensions/.config
+echo "    value: \"postgres://${db_user}:${db_password}@${db_host}/${db_name}\"" >> .ebextensions/.config
 echo "  - option_name: AWS_ACCESS_KEY_ID" >> .ebextensions/.config
 echo "    value: \"${access_key_ses}\"" >> .ebextensions/.config
 echo "  - option_name: AWS_SECRET_ACCESS_KEY" >> .ebextensions/.config
